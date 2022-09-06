ffd6077064e629f986a3618c847c868efcfb5ef3
Lukas Chladek <lukaschladek@gmail.com>
2022-09-06 15:14:43 +0200

    expose svc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,41 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: '',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent
+  implements OnInit, AfterViewInit, AfterContentInit, AfterContentChecked
+{
   view = 'elements';
-  data: any;
-  constructor() {
+
+  @Output() lifeCycle = new EventEmitter();
+  @Output('data') dataEmitter = new EventEmitter();
+  constructor(public data: DataService) {
     this.view = localStorage.getItem('wm-dev-view') || 'elements';
   }
-  ngOnInit(): void {}
+  ngAfterContentChecked(): void {
+    this.emitEvent('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    this.emitEvent('ngAfterContentInit');
+  }
+  ngOnInit(): void {
+    this.emitEvent('ngOnInit');
+    this.dataEmitter.emit(this.data);
+  }
+  ngAfterViewInit(): void {
+    this.emitEvent('ngAfterViewInit');
+  }
 
   toggleView() {
     if (this.view === 'elements') {
@@ -20,5 +44,13 @@ export class AppComponent implements OnInit {
       this.view = 'elements';
     }
     localStorage.setItem('wm-dev-view', this.view);
+  }
+
+  emitEvent(name: string) {
+    this.lifeCycle.emit({
+      event: name,
+      instance: this,
+      service: this.data,
+    });
   }
 }
