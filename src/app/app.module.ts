362233d1c9e1a58cbd96989e4245a5b65d117c13
Lukas Chladek <lukaschladek@gmail.com>
2022-09-06 14:24:38 +0200

    rebuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,17 @@ import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ElementsComponent } from './elements/elements.component';
-import { RootComponent } from './root/root.component';
 import { HttpClientModule } from '@angular/common/http';
 import { createCustomElement } from '@angular/elements';
 @NgModule({
-  declarations: [AppComponent, RootComponent, ElementsComponent],
+  declarations: [AppComponent, ElementsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
 })
 export class AppModule {
   constructor(private injector: Injector) {}
   ngDoBootstrap() {
-    const rootCE = createCustomElement(RootComponent, {
+    const rootCE = createCustomElement(AppComponent, {
       injector: this.injector,
     });
     customElements.define('wm-dev', rootCE);
